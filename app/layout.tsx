1a55859c633ec23c8c6c592c75e7fe81f7285d2b
Bogdan Stanescu <bogdan.stanescu96@gmail.com>
2023-05-07 18:02:39 +0300

    chore: [SESSION] pass provider for root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "@styles/globals.css";
 import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const Metadata = {
   title: "Promptopia",
@@ -10,14 +11,16 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient" />
-        </div>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
 
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
   );
